2dfccc3f38be83e963b9e4ec5b1dd3751931e658
SafdarAliGit <safdar211@gmail.com>
2025-09-20 21:49:45 +0500

    updated

--- farsint/farsint/utils/fetch_recipe.py
+++ farsint/farsint/utils/fetch_recipe.py
@@ -10,18 +10,10 @@ def fetch_recipe(finish_item):
         filters={
             "parent": finish_item
         },
-        fields=["item", "qty", "rate", "amount"],
+        fields=["item", "qty","uom", "rate", "amount"],
         order_by="idx"
     )
-    
-    if recipe:
-        for i in recipe:
-            item = frappe.get_doc("Item", i.item)
-            i.update({
-                "uom": item.stock_uom or "", 
-            })
-        
-
+       
     return {
         "recipe": recipe,
     }
